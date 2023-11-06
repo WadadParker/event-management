8e870c0fdfc7e290d87f2f8e2692d5f504e755da
Wadad Parker <wadadparker5@gmail.com>
2023-11-06 15:10:54 +0530

    feat: event details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom"
 import { SideBar } from "./components/sideBar/sideBar"
 import { Volunteers } from "./pages/volunteers/Volunteers"
 import { Events } from "./pages/events/Events"
+import { EventDetails } from "./features/events/eventDetails/EventDetails"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Volunteers />} />
         <Route path="/events" element={<Events />} />
+        <Route path="/events/:eventId" element={<EventDetails />} />
       </Routes>
 
     </div>
